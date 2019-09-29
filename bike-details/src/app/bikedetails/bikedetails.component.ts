99a74033dd6e383e78dce6e1dbfc2fb7639833a1
ladnikhil <53752444+ladnikhil@users.noreply.github.com>
2019-09-29 19:29:45 +0530

    Update bikedetails.component.ts

--- bike-details/src/app/bikedetails/bikedetails.component.ts
+++ bike-details/src/app/bikedetails/bikedetails.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-bikedetails',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BikedetailsComponent implements OnInit {
 
+   @Input() bikedetails:any = '';
   constructor() { }
 
   ngOnInit() {
