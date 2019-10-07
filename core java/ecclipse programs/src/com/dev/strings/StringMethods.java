768f7ef21ad029110d63fb73c789c4a4c7f7d3b3
ladnikhil <ladnikhil55@gmail.com>
2019-10-07 20:53:03 +0530

    core java

---
+++ core java/ecclipse programs/src/com/dev/strings/StringMethods.java
@@ -0,0 +1,13 @@
+package com.dev.strings;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String str5 = "NIKHIL LAD";
+
+		int length = str5.length();
+
+		System.out.println(str5);
+	}
+
+}
